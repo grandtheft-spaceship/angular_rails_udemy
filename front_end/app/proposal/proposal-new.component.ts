6d634e93ec9a1cb8f1d3aaa04f7cfc5f6785085e
grandtheft-spaceship <justdizon71@gmail.com>
2017-06-15 12:26:37 -0700

    :art: Begins styling new proposal form

--- front_end/app/proposal/proposal-new.component.ts
+++ front_end/app/proposal/proposal-new.component.ts
@@ -3,7 +3,8 @@ import { Proposal } from './proposal';
 @Component({
   moduleId: module.id,
   selector: 'proposal-new',
-  templateUrl: 'proposal-new.component.html'
+  templateUrl: 'proposal-new.component.html',
+  styleUrls: ['proposal-new.component.css']
 })
 
 export class ProposalNewComponent{
